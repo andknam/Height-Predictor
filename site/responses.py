8b0b911b6cd3790bca69b9205fe872591d2500bb
Andrew Nam <andrew22124@gmail.com>
2020-06-09 18:39:08 -0400

    Add files via upload

--- site/responses.py
+++ site/responses.py
@@ -3,10 +3,12 @@ welcome = 'Welcome to Height Predictor. Would you like to fill out information o
 skeletal_low = 'Looks like the skeletal age is too low. Please try again and make a different height prediction.'
 skeletal_high = 'Looks like the skeletal age is too high. Please try again and make a different height prediction.'
 
-skeletal_index_young = 'This skeletal age is too young and not on the graph. Please try again.'
-skeletal_index_old = 'This skeletal age is too old and not on the graph. Please try again.'
+skeletal_index_young = 'Sorry, that bone age is too young.'
+skeletal_index_old = 'Sorry, that bone age is too old.'
 
-height_index_low = 'This height is too small and not on the graph. Please try again.'
-height_index_tall = 'This height is too tall and not on the graph. Please try again.'
+height_index_low = 'Sorry, that height is too low for the bone age.'
+height_index_tall = 'Sorry, that height is too tall for the bone age.'
+
+no_match = 'There were no matches in the reference tables. Please try again.'
 
 testing = 'This works in the testing phase!'
