7f8822ffd948df5b42adaeb857c6703ea28102a4
Andrew Nam <andrew22124@gmail.com>
2020-06-16 00:21:19 -0400

    Update PredictedHeightChart.py

--- site/PredictedHeightChart.py
+++ site/PredictedHeightChart.py
@@ -8,72 +8,86 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET', 'POST'])
 def form_page():
     errors = ''
-    if request.method == 'POST':
-        patient = request.form['patient_input']
-        gender = request.form['gender-checkbox']
-        growth_type = request.form['growth-checkbox']
-        recent_height = None
-        skeletal_year = None
-        skeletal_month = None
+    try:
+        if request.method == 'POST':
+            patient = request.form['patient_input']
+            recent_height = None
+            chronological_year = None
+            chronological_month = None
+            skeletal_year = None
+            skeletal_month = None
+            gender = request.form['gender-checkbox']
 
-        #try-except for height_input
-        try:
-            recent_height = int(request.form['height_input'])
-        except:
-            errors += '{!r} is not a number.\n'.format(request.form['height_input'])
+            #try-except for height_input
+            try:
+                recent_height = int(request.form['height_input'])
+            except:
+                errors += '{!r} is not a number.\n'.format(request.form['height_input'])
 
-        #try-except for skeletal_year_input
-        try:
-            skeletal_year = int(request.form['skeletal_year_input'])
-        except:
-            errors += '{!r} is not a number.\n'.format(request.form['skeletal_year_input'])
+            #try-except for skeletal_year_input
+            try:
+                skeletal_year = int(request.form['skeletal_year_input'])
+            except:
+                errors += '{!r} is not a number.\n'.format(request.form['skeletal_year_input'])
 
-        #try-except for skeletal_month_input
-        try:
-            skeletal_month = int(request.form['skeletal_month_input'])
-        except:
-            errors += '{!r} is not a number.\n'.format(request.form['skeletal_month_input'])
+            #try-except for skeletal_month_input
+            try:
+                skeletal_month = int(request.form['skeletal_month_input'])
+            except:
+                errors += '{!r} is not a number.\n'.format(request.form['skeletal_month_input'])
 
-        if len(errors) == 0:
-            prediction_info = get_prediction_info(gender, recent_height, growth_type, skeletal_year, skeletal_month)
+            #try-except for chronological_year_input
+            try:
+                chronological_year = int(request.form['chronological_year_input'])
+            except:
+                errors += '{!r} is not a number.\n'.format(request.form['chronological_year_input'])
 
-            #make an html for no prediction
-            #add the make another height prediction button to form page
-            #clean code if possible
+            #try-except for chronological_month_input
+            try:
+                chronological_month = int(request.form['chronological_month_input'])
+            except:
+                errors += '{!r} is not a number.\n'.format(request.form['chronological_month_input'])
 
-            if prediction_info[0] == 'skeletal_low':
-                response = responses.skeletal_low
-                return render_template('error_response.html', response=response)
-            elif prediction_info[0] == 'skeletal_high':
-                response = responses.skeletal_high
-                return render_template('error_response.html', response=response)
-            elif prediction_info[0] == 'skeletal_index_young':
-                response = responses.skeletal_index_young
-                return render_template('error_response.html', response=response)
-            elif prediction_info[0] == 'skeletal_index_old':
-                response = responses.skeletal_index_old
-                return render_template('error_response.html', response=response)
-            elif prediction_info[0] == 'height_index_low':
-                response = responses.height_index_low
-                return render_template('error_response.html', response=response)
-            elif prediction_info[0] == 'height_index_tall':
-                response = responses.height_index_tall
-                return render_template('error_response.html', response=response)
+            if len(errors) == 0:
+                prediction_info = get_prediction_info(recent_height, chronological_year, chronological_month, skeletal_year, skeletal_month, gender)
 
-            else:
-                ph = prediction_info[0]
-                pm = prediction_info[1]
+                if prediction_info[0] == 'skeletal_low':
+                    response = responses.skeletal_low
+                    return render_template('error_response.html', response=response)
+                elif prediction_info[0] == 'skeletal_high':
+                    response = responses.skeletal_high
+                    return render_template('error_response.html', response=response)
+                elif prediction_info[0] == 'skeletal_index_young':
+                    response = responses.skeletal_index_young
+                    return render_template('error_response.html', response=response)
+                elif prediction_info[0] == 'skeletal_index_old':
+                    response = responses.skeletal_index_old
+                    return render_template('error_response.html', response=response)
+                elif prediction_info[0] == 'height_index_low':
+                    response = responses.height_index_low
+                    return render_template('error_response.html', response=response)
+                elif prediction_info[0] == 'height_index_tall':
+                    response = responses.height_index_tall
+                    return render_template('error_response.html', response=response)
+                elif prediction_info[0] == 'chronological_old':
+                    response = responses.chronological_old
+                    return render_template('error_response.html', response=response)
 
-                rh = recent_height
-                sy = skeletal_year
-                sm = skeletal_month
-                p = patient
-
-                if gender == 'male':
-                    return render_template('valid_male_response.html', rh=rh, sy=sy, sm=sm, p=p, ph=ph, pm=pm)
                 else:
-                    return render_template('valid_female_response.html', rh=rh, sy=sy, sm=sm, p=p, ph=ph, pm=pm)
+                    ph = prediction_info[0]
+                    pm = prediction_info[1]
 
+                    rh = recent_height
+                    sy = skeletal_year
+                    sm = skeletal_month
+                    p = patient
+
+                    if gender == 'male':
+                        return render_template('valid_male_response.html', rh=rh, sy=sy, sm=sm, p=p, ph=ph, pm=pm)
+                    else:
+                        return render_template('valid_female_response.html', rh=rh, sy=sy, sm=sm, p=p, ph=ph, pm=pm)
+
+    except:
+        errors += 'Something went wrong :( Please reload the page and try again'
+        
     return render_template('form_page.html', errors=errors)
-
-
