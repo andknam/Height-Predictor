285bc3faa87ae86de9f2e4e8358587bd581c5d41
Andrew Nam <andrew22124@gmail.com>
2020-07-02 20:17:32 -0400

    Update responses.py

--- alexa-skill/lambda/responses.py
+++ alexa-skill/lambda/responses.py
@@ -3,10 +3,31 @@ welcome = 'Welcome to Height Predictor. Would you like to fill out information o
 skeletal_low = 'Looks like the skeletal age is too low. Please try again and make a different height prediction.'
 skeletal_high = 'Looks like the skeletal age is too high. Please try again and make a different height prediction.'
 
-skeletal_index_young = 'This skeletal age is too young and not on the graph. Please try again.'
-skeletal_index_old = 'This skeletal age is too old and not on the graph. Please try again.'
+skeletal_index_young = 'Sorry, that bone age is too young.'
+skeletal_index_old = 'Sorry, that bone age is too old.'
 
-height_index_low = 'This height is too small and not on the graph. Please try again.'
-height_index_tall = 'This height is too tall and not on the graph. Please try again.'
+height_index_low = 'Sorry, that height is too low for the bone age.'
+height_index_tall = 'Sorry, that height is too tall for the bone age.'
+
+chronological_young = 'Sorry, that chronological age is too young.'
+chronological_old = 'Sorry, that chronological age is too old.'
+
+male_brush = 'I personally interpreted the skeletal age as {} years {} months (using Brush foundation means +/- 2 SD). \
+    This skeletal age is {} based on his current age of {} years {} months and 2 standard deviations \
+    reference of {} months. The adult height prediction is {} inches and he has apparently completed \
+    {}% of his growth.'
+
+female_brush = 'I personally interpreted the skeletal age as {} years {} months (using Brush foundation means +/- 2 SD). \
+    This skeletal age is {} based on her current age of {} years {} months and 2 standard deviations \
+    reference of {} months. The adult height prediction is {} inches and she has apparently completed \
+    {}% of her growth.'
+
+male_one_year = 'I personally interpreted the skeletal age as {} years {} months (using 12 months from chronological age) \
+    This skeletal age is {} based on his current age of {} years {} months. The adult height prediction \
+    is {} inches and he has apparently completed {}% of his growth.' 
+
+female_one_year = 'I personally interpreted the skeletal age as {} years {} months (using 12 months from chronological age) \
+    This skeletal age is {} based on her current age of {} years {} months. The adult height prediction \
+    is {} inches and she has apparently completed {}% of her growth.' 
 
 testing = 'This works in the testing phase!'
